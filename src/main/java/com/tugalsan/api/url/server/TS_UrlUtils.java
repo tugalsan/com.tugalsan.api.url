20e31951ecd86b576804f9efbee001ba6cf1df8f
tugalsan karabacak <tugalsan@gmail.com>
2022-08-17 13:15:31 +0300

    bug fix

--- src/main/java/com/tugalsan/api/url/server/TS_UrlUtils.java
+++ src/main/java/com/tugalsan/api/url/server/TS_UrlUtils.java
@@ -7,6 +7,7 @@ import javax.servlet.http.*;
 import com.tugalsan.api.log.server.*;
 import com.tugalsan.api.url.client.*;
 import com.tugalsan.api.file.server.*;
+import com.tugalsan.api.pack.client.*;
 import com.tugalsan.api.unsafe.client.*;
 
 public class TS_UrlUtils {
@@ -92,8 +93,8 @@ public class TS_UrlUtils {
         return TGS_UnSafe.compile(() -> TGS_Url.of(file.toUri().toURL().toExternalForm()), exception -> null);
     }
 
-    public static Path toPath(TGS_Url url) {
-        return TS_PathUtils.toPath(url.toString());
+    public static TGS_Pack2<Path, Exception> toPathAndError(TGS_Url url) {
+        return TS_PathUtils.toPathAndError(url.toString());
     }
 
     public static boolean isUrl(CharSequence str) {
