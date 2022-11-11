2f9a266ec8fb70379003612c361d54cd46968242
tugalsan karabacak <tugalsan@gmail.com>
2022-11-11 15:35:16 +0300

    parallel optionaized

--- src/main/java/com/tugalsan/api/url/client/TGC_UrlDDosUtils.java
+++ src/main/java/com/tugalsan/api/url/client/TGC_UrlDDosUtils.java
@@ -5,7 +5,7 @@ import com.google.gwt.http.client.RequestBuilder;
 import com.google.gwt.http.client.RequestCallback;
 import com.google.gwt.http.client.Response;
 import com.google.gwt.http.client.URL;
-import com.tugalsan.api.random.server.TS_RandomUtils;
+import com.tugalsan.api.random.client.TGS_RandomUtils;
 import com.tugalsan.api.stream.client.TGS_StreamUtils;
 import com.tugalsan.api.unsafe.client.TGS_UnSafe;
 import com.tugalsan.api.url.client.parser.TGS_UrlParser;
@@ -14,7 +14,9 @@ public class TGC_UrlDDosUtils {
 
     public static void attack(TGS_Url url, RequestCallback requestCallback) {
         var parser = TGS_UrlParser.of(url);
-        parser.quary.setParameterValueUrlSafe("p", TGS_UrlQueryUtils.readable_2_Param64UrlSafe(String.valueOf(TS_RandomUtils.nextInt(0, Integer.MAX_VALUE))));
+        parser.quary.setParameterValueUrlSafe("p", TGS_UrlQueryUtils.readable_2_Param64UrlSafe(
+                String.valueOf(TGS_RandomUtils.nextInt(0, Integer.MAX_VALUE)))
+        );
         TGS_UnSafe.execute(() -> {
             var builder = new RequestBuilder(RequestBuilder.GET, URL.encode(parser.toString()));
             builder.sendRequest(null, new RequestCallback() {
