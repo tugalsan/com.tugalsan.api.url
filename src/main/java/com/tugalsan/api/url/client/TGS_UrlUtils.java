fcdd6cdcfad208c936e46f2b77368c5041cf1a3e
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-05-11 06:57:04 +0300

    Update TGS_UrlUtils.java

--- src/main/java/com/tugalsan/api/url/client/TGS_UrlUtils.java
+++ src/main/java/com/tugalsan/api/url/client/TGS_UrlUtils.java
@@ -11,18 +11,30 @@ public class TGS_UrlUtils {
 
     public static boolean isChildOf(TGS_Url base, TGS_Url self) {
         if (self.toString().startsWith("../")) {
+            System.out.println("isChildOf .. " + self);
             return false;
         }
-        if (self.toString().startsWith("http")) {
+        if (self.toString().startsWith("http:")) {
+            System.out.println("isChildOf http " + self);
+            return false;
+        }
+        if (self.toString().startsWith("https:")) {
+            System.out.println("isChildOf http " + self);
             return false;
         }
         if (self.toString().startsWith("ftp")) {
+            System.out.println("isChildOf .. " + self);
             return false;
         }
-        if (self.toString().startsWith("file")) {
+        if (self.toString().startsWith("ftp:")) {
+            System.out.println("isChildOf .. " + self);
             return false;
         }
-        return self.toString().startsWith(base.toString());
+        if (self.toString().startsWith("file:")) {
+            System.out.println("isChildOf ftp " + self);
+            return false;
+        }
+        return true;
     }
 
     public static TGS_Url trimAnchor(TGS_Url url) {
