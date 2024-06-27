1a83c98069f72c83abbc10e279f7e6af58a754a1
tugalsan karabacak <tugalsan@users.noreply.github.com>
2024-06-27 12:07:31 +0300

    .

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,7 @@ module com.tugalsan.api.url {
     requires gwt.user;
     requires java.net.http;
     
-    requires com.tugalsan.api.callable;
+    requires com.tugalsan.api.function;
     requires com.tugalsan.api.unsafe;
     requires com.tugalsan.api.log;
     requires com.tugalsan.api.network;
